db27340df8dabdec48ca2ce9a252510632fb6b43
LuckyBelieve <luckybelieve10@gmail.com>
2025-01-14 15:40:29 +0200

    feat:progress newPosting

--- app/(main)/newPost.jsx
+++ app/(main)/newPost.jsx
@@ -1,15 +1,129 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import ScreenWrapper from '../../components/ScreenWrapper';
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useRef, useState } from "react";
+import ScreenWrapper from "../../components/ScreenWrapper";
+import Header from "../../components/Header";
+import { theme } from "../../constants/theme";
+import { hp, wp } from "../../helpers/common";
+import { useAuth } from "../../context/AuthContext";
+import Avatar from "../../components/Avatar";
+import { useRouter } from "expo-router";
+import RichTextEditor from "../../components/RichTextEditor";
 
 const NewPost = () => {
+  const { user } = useAuth();
+  const bodyRef = useRef("");
+    const editorRef = useRef(null);
+    const router = useRouter();
+    const [loading,setLoading] = useState(false);
+    const [file,setFile] = useState(null)
   return (
-   <ScreenWrapper>
-    <Text>New post</Text>
-   </ScreenWrapper>
-  )
-}
+    <ScreenWrapper bg={"white"}>
+      <View style={styles.container}>
+        <Header title={"Create Post"} />
+        <ScrollView contentContainerStyle={{ gap: 20 }}>
+          {/* avatar */}
+          <View style={styles.header}>
+            <Avatar
+              uri={user?.image}
+              size={hp(6.5)}
+              rounded={theme.radius.xl}
+            />
+            <View style={{ gap: 2 }}>
+              <Text style={styles.username}>{user && user?.name}</Text>
+              <Text style={styles.publicText}>Public</Text>
+            </View>
+          </View>
+          <View style={styles.textEditor}>
+            <RichTextEditor editorRef={editorRef} onChange = {value=>bodyRef.current = value}/>
+          </View>
+          <View style={styles.media}>
+            <Text style={styles.addImage}>Add to your post!</Text>
+            <View style={styles.mediaIcons}>
+                {/* <TouchableOpacity></TouchableOpacity> */}
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    </ScreenWrapper>
+  );
+};
 
 export default NewPost;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginBottom: 30,
+    paddingHorizontal: wp(4),
+    gap: 15,
+  },
+  title: {
+    fontSize: hp(2.5),
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+    textAlign: "center",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  username: {
+    fontSize: hp(2.2),
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+  },
+  avatar: {
+    height: hp(6.5),
+    width: hp(6.5),
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.1)",
+  },
+  publicText: {
+    fontSize: hp(1.7),
+    fontWeight: theme.fonts.medium,
+    color: theme.colors.textLight,
+  },
+  textEditor: {
+    // marginTop:10
+  },
+  media: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderWidth: 1.5,
+    padding: 12,
+    paddingHorizontal: 18,
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+    borderColor: theme.colors.gray,
+  },
+  mediaIcons: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+  },
+  addImage: {
+    fontSize: hp(1.9),
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+  },
+  imageIcon: {
+    borderRadius: theme.radius.md,
+  },
+  file: {
+    height: hp(30),
+    width: "100%",
+    borderRadius: theme.radius.xl,
+    overflow: "hidden",
+    borderCurve: "continuous",
+  },
+  video: {},
+  closeIcon: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+  },
+});
