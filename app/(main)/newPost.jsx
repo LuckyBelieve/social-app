ff065d4c64ae6dcf1882d919c42a738925191d1d
LuckyBelieve <luckybelieve10@gmail.com>
2025-01-14 21:57:14 +0200

    feat: complete newPost layout

--- app/(main)/newPost.jsx
+++ app/(main)/newPost.jsx
@@ -1,4 +1,11 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useRef, useState } from "react";
 import ScreenWrapper from "../../components/ScreenWrapper";
 import Header from "../../components/Header";
@@ -8,19 +15,74 @@ import { useAuth } from "../../context/AuthContext";
 import Avatar from "../../components/Avatar";
 import { useRouter } from "expo-router";
 import RichTextEditor from "../../components/RichTextEditor";
+import Icon from "../../assets/icons";
+import Button from "../../components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { Image } from "expo-image";
+import { getSupabaseUrl } from "../../services/imageService";
+import { Video } from "expo-av";
 
 const NewPost = () => {
   const { user } = useAuth();
   const bodyRef = useRef("");
-    const editorRef = useRef(null);
-    const router = useRouter();
-    const [loading,setLoading] = useState(false);
-    const [file,setFile] = useState(null)
+  const editorRef = useRef(null);
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState(null);
+
+  const onPick = async (isImage) => {
+    let mediaConfig = {
+      mediaTypes: "images",
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 0.7,
+    };
+
+    if (!isImage) {
+      mediaConfig = {
+        mediaTypes: "videos",
+        allowsEditing: true,
+      };
+    }
+    const result = await ImagePicker.launchImageLibraryAsync(mediaConfig);
+    if (!result.canceled) {
+      setFile(result.assets[0]);
+    }
+  };
+
+  const isLocalFile = (file) => {
+    if (typeof file == "object") return true;
+    return false;
+  };
+
+  const getFileType = (file) => {
+    if (!file) return null;
+    if (isLocalFile(file)) {
+      return file.type;
+    }
+    // check type from remote sites
+    if (file.includes("postImage")) {
+      return "image";
+    }
+    return "video";
+  };
+
+  const getFileUri = (file) => {
+    if (!file) return null;
+    if (isLocalFile(file)) return file.uri;
+    return getSupabaseUrl(file)?.uri;
+  };
+
+  //   submission
+  const onSubmit = async () => {};
   return (
     <ScreenWrapper bg={"white"}>
       <View style={styles.container}>
         <Header title={"Create Post"} />
-        <ScrollView contentContainerStyle={{ gap: 20 }}>
+        <ScrollView
+          contentContainerStyle={{ gap: 20 }}
+          showsVerticalScrollIndicator={false}
+        >
           {/* avatar */}
           <View style={styles.header}>
             <Avatar
@@ -34,15 +96,54 @@ const NewPost = () => {
             </View>
           </View>
           <View style={styles.textEditor}>
-            <RichTextEditor editorRef={editorRef} onChange = {value=>bodyRef.current = value}/>
+            <RichTextEditor
+              editorRef={editorRef}
+              onChange={(value) => (bodyRef.current = value)}
+            />
           </View>
+          {file && (
+            <View style={styles.file}>
+              {getFileType(file) === "video" ? (
+                <Video
+                  style={{ flex: 1 }}
+                  source={{
+                    uri: getFileUri(file),
+                  }}
+                  useNativeControls
+                  resizeMode="cover"
+                  isLooping
+                />
+              ) : (
+                <Image
+                  source={{ uri: getFileUri(file) }}
+                  contentFit="cover"
+                  style={{ flex: 1 }}
+                />
+              )}
+              <Pressable style={styles.closeIcon} onPress={() => setFile(null)}>
+                <Icon name={"delete"} size={20} color="white" />
+              </Pressable>
+            </View>
+          )}
           <View style={styles.media}>
             <Text style={styles.addImage}>Add to your post!</Text>
             <View style={styles.mediaIcons}>
-                {/* <TouchableOpacity></TouchableOpacity> */}
+              <TouchableOpacity onPress={() => onPick(true)}>
+                <Icon name={"image"} size={30} color={theme.colors.dark} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => onPick(false)}>
+                <Icon name={"video"} size={30} color={theme.colors.dark} />
+              </TouchableOpacity>
             </View>
           </View>
         </ScrollView>
+        <Button
+          buttonStyle={{ height: hp(6.2) }}
+          title="Post"
+          loading={loading}
+          hasShadow={false}
+          onpress={onSubmit}
+        />
       </View>
     </ScreenWrapper>
   );
@@ -125,5 +226,8 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 10,
     right: 10,
+    padding: 7,
+    borderRadius: 50,
+    backgroundColor: "rgba(255,0,0,0.6)",
   },
 });
