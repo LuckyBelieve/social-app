27f62dd1c3636ed7ff394d50cd2f1624698d49cd
LuckyBelieve <luckybelieve10@gmail.com>
2025-01-15 11:20:23 +0200

    feat:complete postCard

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,39 +3,44 @@ import { AuthProvider, useAuth } from "../context/AuthContext";
 import { useEffect } from "react";
 import { supabase } from "../lib/superbase";
 import { userService } from "../services/userService";
+import { LogBox } from "react-native";
 
-
-const _layout = ()=>{
-    return(
-        <AuthProvider>
-            <MainLayout />
-        </AuthProvider>
-    )
-}
+LogBox.ignoreLogs([
+  "Warning: TNodeChildrenRenderer",
+  "Warning: MemoizedTNodeRenderer",
+  "Warning: TRenderEngineProvider",
+]);
+const _layout = () => {
+  return (
+    <AuthProvider>
+      <MainLayout />
+    </AuthProvider>
+  );
+};
 
 const MainLayout = () => {
-    const { setAuth,setUserData } = useAuth();
-    const router = useRouter();
+  const { setAuth, setUserData } = useAuth();
+  const router = useRouter();
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((_event, session) => {
-      if(session){
+      if (session) {
         setAuth(session?.user);
-        updateUserData(session?.user,session?.user?.email)
+        updateUserData(session?.user, session?.user?.email);
         router.replace("/home");
-      }else{
+      } else {
         setAuth(null);
         router.replace("/Welcome");
       }
-    })
-  }, [])
+    });
+  }, []);
 
-  const updateUserData = async (user,email)=>{
+  const updateUserData = async (user, email) => {
     let res = await userService(user?.id);
-    if(res.success){
-      setUserData({...res.data,email})
+    if (res.success) {
+      setUserData({ ...res.data, email });
     }
-  }
-    return <Stack screenOptions={{ headerShown: false }}/>
   };
+  return <Stack screenOptions={{ headerShown: false }} />;
+};
 export default _layout;
